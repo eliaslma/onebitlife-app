55421009719675ac3d8382ea69bda2c48270e64e
Elias Lima <sci.eliaslima@gmail.com>
2022-12-03 20:01:48 -0300

    Added conditional rendering of pages

--- src/routes/HomePage.jsx
+++ src/routes/HomePage.jsx
@@ -1,15 +1,13 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
-import Start from "../pages/Start";
 import AppExplanation from "../pages/AppExplanation";
 import Home from "../pages/Home";
 import HabitPage from "../pages/HabitPage";
 
-
 const Stack = createNativeStackNavigator();
 
-export default function AllPages() {
+export default function HomePage() {
   return (
     <NavigationContainer>
       <Stack.Navigator
@@ -17,10 +15,9 @@ export default function AllPages() {
           headerShown: false,
         }}
       >
-        <Stack.Screen name="Start" component={Start} />
-        <Stack.Screen name="AppExplanation" component={AppExplanation} />
         <Stack.Screen name="Home" component={Home} />
         <Stack.Screen name="HabitPage" component={HabitPage} />
+        <Stack.Screen name="AppExplanation" component={AppExplanation} />
       </Stack.Navigator>
     </NavigationContainer>
   );
